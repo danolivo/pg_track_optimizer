0a8a9459cd1571720efcb300651ca7e737af9600
Andrei V. Lepikhov <lepihov@gmail.com>
2025-12-18 14:16:52 +0100

    Improve VIEW: show only database-relevant data

--- pg_track_optimizer--0.1.sql
+++ pg_track_optimizer--0.1.sql
@@ -19,7 +19,12 @@ RETURNS setof record
 AS 'MODULE_PATHNAME', 'to_show_data'
 LANGUAGE C STRICT VOLATILE;
 
-CREATE VIEW pg_track_optimizer AS SELECT * FROM pg_track_optimizer();
+CREATE VIEW pg_track_optimizer AS SELECT
+  t.queryid, t.query, t.avg_error, t.rms_error, t.twa_error,
+  t.wca_error, t.evaluated_nodes, t.plan_nodes, t.exec_time, t.nexecs
+FROM pg_track_optimizer() t, pg_database d
+WHERE t.dboid = d.oid AND datname = current_database();
+COMMENT ON VIEW pg_track_optimizer IS 'query tracking data for current database';
 
 CREATE FUNCTION pg_track_optimizer_flush()
 RETURNS VOID
