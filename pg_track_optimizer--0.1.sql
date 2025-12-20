1b469ca004e785c00e8b4c1b533219caef2ea335
Andrei V. Lepikhov <lepihov@gmail.com>
2025-12-20 16:38:45 +0100

    Introduce statistics base type

--- pg_track_optimizer--0.1.sql
+++ pg_track_optimizer--0.1.sql
@@ -2,6 +2,122 @@
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
 \echo Use "CREATE EXTENSION pg_track_optimizer" to load this file. \quit
 
+
+CREATE TYPE statistics;
+
+-- Input/Output functions
+CREATE FUNCTION statistics_in(cstring)
+    RETURNS statistics
+    AS 'MODULE_PATHNAME'
+    LANGUAGE C IMMUTABLE STRICT;
+
+CREATE FUNCTION statistics_out(statistics)
+    RETURNS cstring
+    AS 'MODULE_PATHNAME'
+    LANGUAGE C IMMUTABLE STRICT;
+
+-- Binary I/O functions
+CREATE FUNCTION statistics_recv(internal)
+    RETURNS statistics
+    AS 'MODULE_PATHNAME'
+    LANGUAGE C IMMUTABLE STRICT;
+
+CREATE FUNCTION statistics_send(statistics)
+    RETURNS bytea
+    AS 'MODULE_PATHNAME'
+    LANGUAGE C IMMUTABLE STRICT;
+
+-- Create the type
+CREATE TYPE statistics (
+    INTERNALLENGTH = 48,
+    INPUT = statistics_in,
+    OUTPUT = statistics_out,
+    RECEIVE = statistics_recv,
+    SEND = statistics_send,
+    ALIGNMENT = double
+);
+
+COMMENT ON TYPE statistics IS 'Incremental statistics type using Welford''s algorithm';
+
+--
+-- Initialization operator (double precision -> statistics)
+--
+
+CREATE FUNCTION statistics_init(double precision)
+    RETURNS statistics
+    AS 'MODULE_PATHNAME'
+    LANGUAGE C IMMUTABLE STRICT;
+COMMENT ON FUNCTION statistics_init(double precision) IS 'Initialize statistics from a single value';
+
+-- Cast from double precision to statistics
+CREATE CAST (double precision AS statistics)
+    WITH FUNCTION statistics_init(double precision)
+    AS IMPLICIT;
+
+--
+-- Addition operator (statistics + double precision)
+--
+
+CREATE FUNCTION statistics_add(statistics, double precision)
+    RETURNS statistics
+    AS 'MODULE_PATHNAME'
+    LANGUAGE C IMMUTABLE STRICT;
+COMMENT ON FUNCTION statistics_add(statistics, double precision) IS 'Add a new value to statistics using Welford''s algorithm';
+
+CREATE OPERATOR + (
+    LEFTARG = statistics,
+    RIGHTARG = double precision,
+    FUNCTION = statistics_add,
+    COMMUTATOR = +
+);
+COMMENT ON OPERATOR + (statistics, double precision) IS 'Add a value to statistics';
+
+--
+-- Accessor functions
+--
+
+CREATE FUNCTION stats_count(statistics)
+    RETURNS bigint
+    AS 'MODULE_PATHNAME', 'statistics_get_count'
+    LANGUAGE C IMMUTABLE STRICT;
+
+COMMENT ON FUNCTION stats_count(statistics) IS 'Get the count from statistics';
+
+CREATE FUNCTION stats_mean(statistics)
+    RETURNS double precision
+    AS 'MODULE_PATHNAME', 'statistics_get_mean'
+    LANGUAGE C IMMUTABLE STRICT;
+
+COMMENT ON FUNCTION stats_mean(statistics) IS 'Get the mean from statistics';
+
+CREATE FUNCTION stats_variance(statistics)
+    RETURNS double precision
+    AS 'MODULE_PATHNAME', 'statistics_get_variance'
+    LANGUAGE C IMMUTABLE STRICT;
+
+COMMENT ON FUNCTION stats_variance(statistics) IS 'Get the variance from statistics';
+
+CREATE FUNCTION stats_stddev(statistics)
+    RETURNS double precision
+    AS 'MODULE_PATHNAME', 'statistics_get_stddev'
+    LANGUAGE C IMMUTABLE STRICT;
+
+COMMENT ON FUNCTION stats_stddev(statistics) IS 'Get the standard deviation from statistics';
+
+CREATE FUNCTION stats_min(statistics)
+    RETURNS double precision
+    AS 'MODULE_PATHNAME', 'statistics_get_min'
+    LANGUAGE C IMMUTABLE STRICT;
+
+COMMENT ON FUNCTION stats_min(statistics) IS 'Get the minimum from statistics';
+
+CREATE FUNCTION stats_max(statistics)
+    RETURNS double precision
+    AS 'MODULE_PATHNAME', 'statistics_get_max'
+    LANGUAGE C IMMUTABLE STRICT;
+
+COMMENT ON FUNCTION stats_max(statistics) IS 'Get the maximum from statistics';
+
 CREATE FUNCTION pg_track_optimizer(
 	OUT dboid			Oid,
 	OUT queryid			bigint,
---
+++ statistics.c
@@ -0,0 +1,273 @@
+/*
+ * stat_type.c - base type for incremental statistics type
+ *
+ * This implements a 'statistics' base type that maintains
+ * running statistics using Welford's algorithm for numerical stability.
+ */
+
+#include "postgres.h"
+#include "fmgr.h"
+#include "libpq/pqformat.h"
+#include "utils/builtins.h"
+#include <math.h>
+
+/*
+ * Internal representation of statistics
+ * Uses Welford's algorithm to maintain numerically stable running statistics
+ */
+typedef struct Statistics
+{
+    int32       vl_len_;        /* varlena header (do not touch directly!) */
+    int64       count;          /* number of values */
+    double      mean;           /* running mean */
+    double      m2;             /* sum of squared differences from mean (for variance) */
+    double      min;            /* minimum value */
+    double      max;            /* maximum value */
+} Statistics;
+
+/* Macros for easier access */
+#define DatumGetStatisticsP(X)      ((Statistics *) PG_DETOAST_DATUM(X))
+#define StatisticsPGetDatum(X)      PointerGetDatum(X)
+#define PG_GETARG_STATISTICS_P(n)   DatumGetStatisticsP(PG_GETARG_DATUM(n))
+#define PG_RETURN_STATISTICS_P(x)   return StatisticsPGetDatum(x)
+
+/* Function declarations */
+PG_FUNCTION_INFO_V1(statistics_in);
+PG_FUNCTION_INFO_V1(statistics_out);
+PG_FUNCTION_INFO_V1(statistics_recv);
+PG_FUNCTION_INFO_V1(statistics_send);
+PG_FUNCTION_INFO_V1(statistics_init);
+PG_FUNCTION_INFO_V1(statistics_add);
+PG_FUNCTION_INFO_V1(statistics_get_count);
+PG_FUNCTION_INFO_V1(statistics_get_mean);
+PG_FUNCTION_INFO_V1(statistics_get_variance);
+PG_FUNCTION_INFO_V1(statistics_get_stddev);
+PG_FUNCTION_INFO_V1(statistics_get_min);
+PG_FUNCTION_INFO_V1(statistics_get_max);
+#include "varatt.h"
+/*
+ * Input function: converts text representation to internal format
+ * Format: (count:N,mean:M,min:MIN,max:MAX,variance:V)
+ */
+Datum
+statistics_in(PG_FUNCTION_ARGS)
+{
+    char       *str = PG_GETARG_CSTRING(0);
+    Statistics *result;
+    int64       count;
+    double      mean, min_val, max_val, variance;
+    int         nfields;
+
+    /* Parse the input string */
+    nfields = sscanf(str, "(count:"INT64_FORMAT",mean:%lf,min:%lf,max:%lf,variance:%lf)",
+                     &count, &mean, &min_val, &max_val, &variance);
+
+    if (nfields != 5)
+        ereport(ERROR,
+                (errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
+                 errmsg("invalid input syntax for type statistics: \"%s\"", str),
+                 errhint("Expected format: (count:N,mean:M,min:MIN,max:MAX,variance:V)")));
+
+    if (count < 0)
+        ereport(ERROR,
+                (errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
+                 errmsg("count must be non-negative")));
+
+    /* Allocate and initialize the result */
+    result = (Statistics *) palloc(sizeof(Statistics));
+    SET_VARSIZE(result, sizeof(Statistics));
+
+    result->count = count;
+    result->mean = mean;
+    result->min = min_val;
+    result->max = max_val;
+
+    /* Convert variance to m2 (sum of squared differences) */
+    if (count > 1)
+        result->m2 = variance * (count - 1);
+    else
+        result->m2 = 0.0;
+
+    PG_RETURN_STATISTICS_P(result);
+}
+
+/*
+ * Output function: converts internal format to text representation
+ */
+Datum
+statistics_out(PG_FUNCTION_ARGS)
+{
+    Statistics *stats = PG_GETARG_STATISTICS_P(0);
+    char       *result;
+    double      variance;
+
+    /* Calculate variance from m2 */
+    if (stats->count > 1)
+        variance = stats->m2 / (stats->count - 1);
+    else
+        variance = 0.0;
+
+    result = psprintf("(count:%lld,mean:%.15g,min:%.15g,max:%.15g,variance:%.15g)",
+                      stats->count, stats->mean, stats->min, stats->max, variance);
+
+    PG_RETURN_CSTRING(result);
+}
+
+/*
+ * Binary input function
+ */
+Datum
+statistics_recv(PG_FUNCTION_ARGS)
+{
+    StringInfo  buf = (StringInfo) PG_GETARG_POINTER(0);
+    Statistics *result;
+
+    result = (Statistics *) palloc(sizeof(Statistics));
+    SET_VARSIZE(result, sizeof(Statistics));
+
+    result->count = pq_getmsgint64(buf);
+    result->mean = pq_getmsgfloat8(buf);
+    result->m2 = pq_getmsgfloat8(buf);
+    result->min = pq_getmsgfloat8(buf);
+    result->max = pq_getmsgfloat8(buf);
+
+    PG_RETURN_STATISTICS_P(result);
+}
+
+/*
+ * Binary output function
+ */
+Datum
+statistics_send(PG_FUNCTION_ARGS)
+{
+    Statistics *stats = PG_GETARG_STATISTICS_P(0);
+    StringInfoData buf;
+
+    pq_begintypsend(&buf);
+    pq_sendint64(&buf, stats->count);
+    pq_sendfloat8(&buf, stats->mean);
+    pq_sendfloat8(&buf, stats->m2);
+    pq_sendfloat8(&buf, stats->min);
+    pq_sendfloat8(&buf, stats->max);
+
+    PG_RETURN_BYTEA_P(pq_endtypsend(&buf));
+}
+
+/*
+ * Initialize statistics from a single value
+ * Usage: statistics_init(42.5) returns a statistics object with count=1
+ */
+Datum
+statistics_init(PG_FUNCTION_ARGS)
+{
+    double      value = PG_GETARG_FLOAT8(0);
+    Statistics *result;
+
+    result = (Statistics *) palloc(sizeof(Statistics));
+    SET_VARSIZE(result, sizeof(Statistics));
+
+    result->count = 1;
+    result->mean = value;
+    result->m2 = 0.0;      /* No variance with single value */
+    result->min = value;
+    result->max = value;
+
+    PG_RETURN_STATISTICS_P(result);
+}
+
+/*
+ * Add a new value to existing statistics using Welford's algorithm
+ * Usage: stats + 42.5 returns updated statistics
+ */
+Datum
+statistics_add(PG_FUNCTION_ARGS)
+{
+    Statistics *stats = PG_GETARG_STATISTICS_P(0);
+    double      value = PG_GETARG_FLOAT8(1);
+    Statistics *result;
+    double      delta, delta2;
+    int64       new_count;
+
+    /* Allocate result */
+    result = (Statistics *) palloc(sizeof(Statistics));
+    SET_VARSIZE(result, sizeof(Statistics));
+
+    /* Welford's algorithm for incremental mean and variance */
+    new_count = stats->count + 1;
+    delta = value - stats->mean;
+
+    result->count = new_count;
+    result->mean = stats->mean + delta / new_count;
+
+    delta2 = value - result->mean;
+    result->m2 = stats->m2 + delta * delta2;
+
+    /* Update min/max */
+    result->min = (value < stats->min) ? value : stats->min;
+    result->max = (value > stats->max) ? value : stats->max;
+
+    PG_RETURN_STATISTICS_P(result);
+}
+
+/*
+ * Accessor functions to get individual statistics
+ */
+
+Datum
+statistics_get_count(PG_FUNCTION_ARGS)
+{
+    Statistics *stats = PG_GETARG_STATISTICS_P(0);
+    PG_RETURN_INT64(stats->count);
+}
+
+Datum
+statistics_get_mean(PG_FUNCTION_ARGS)
+{
+    Statistics *stats = PG_GETARG_STATISTICS_P(0);
+    PG_RETURN_FLOAT8(stats->mean);
+}
+
+Datum
+statistics_get_variance(PG_FUNCTION_ARGS)
+{
+    Statistics *stats = PG_GETARG_STATISTICS_P(0);
+    double      variance;
+
+    if (stats->count > 1)
+        variance = stats->m2 / (stats->count - 1);
+    else
+        variance = 0.0;
+
+    PG_RETURN_FLOAT8(variance);
+}
+
+Datum
+statistics_get_stddev(PG_FUNCTION_ARGS)
+{
+    Statistics *stats = PG_GETARG_STATISTICS_P(0);
+    double      variance, stddev;
+
+    if (stats->count > 1)
+    {
+        variance = stats->m2 / (stats->count - 1);
+        stddev = sqrt(variance);
+    }
+    else
+        stddev = 0.0;
+
+    PG_RETURN_FLOAT8(stddev);
+}
+
+Datum
+statistics_get_min(PG_FUNCTION_ARGS)
+{
+    Statistics *stats = PG_GETARG_STATISTICS_P(0);
+    PG_RETURN_FLOAT8(stats->min);
+}
+
+Datum
+statistics_get_max(PG_FUNCTION_ARGS)
+{
+    Statistics *stats = PG_GETARG_STATISTICS_P(0);
+    PG_RETURN_FLOAT8(stats->max);
+}
