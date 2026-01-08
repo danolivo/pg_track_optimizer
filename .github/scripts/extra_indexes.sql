2be2faf7d50071a23a2c73475bda525b87d0381e
Andrei V. Lepikhov <lepihov@gmail.com>
2026-01-08 21:17:58 +0100

    Correct extra indexes

--- .github/scripts/extra_indexes.sql
+++ .github/scripts/extra_indexes.sql
@@ -6,21 +6,15 @@
 
 CREATE INDEX aka_name_idx_1 ON aka_name (info);
 CREATE INDEX aka_name_idx_2 ON aka_name (name);
-CREATE INDEX aka_title_idx_1 ON aka_title (info,note);
-CREATE INDEX cast_info_idx_1 ON cast_info (note);
-CREATE INDEX company_name_idx_1 ON company_name (country_code);
-CREATE INDEX complete_cast_idx_1 ON complete_cast (info);
-CREATE INDEX complete_cast_idx_2 ON complete_cast (info,note);
-CREATE INDEX info_type_idx_1 ON info_type (info);
-CREATE INDEX keyword_idx_1 ON keyword (keyword);
-CREATE INDEX movie_companies_idx_1 ON movie_companies (country_code);
-CREATE INDEX movie_companies_idx_2 ON movie_companies (info);
-CREATE INDEX movie_companies_idx_3 ON movie_companies (info,note);
-CREATE INDEX movie_companies_idx_4 ON movie_companies (note);
-CREATE INDEX movie_info_idx_1 ON movie_info (info);
 CREATE INDEX movie_info_idx_idx_1 ON movie_info_idx (info);
-CREATE INDEX movie_keyword_idx_1 ON movie_keyword (info);
-CREATE INDEX movie_keyword_idx_2 ON movie_keyword (keyword);
-CREATE INDEX movie_keyword_idx_3 ON movie_keyword (note);
-CREATE INDEX movie_link_idx_1 ON movie_link (note);
 CREATE INDEX title_idx_1 ON title (production_year);
+CREATE INDEX movie_companies_idx_1 ON movie_companies (country_code);
+
+CREATE EXTENSION IF NOT EXISTS pg_trgm;
+
+CREATE INDEX idx_movie_companies ON movie_companies USING gin (note gin_trgm_ops);
+CREATE INDEX cast_info_idx_1 ON cast_info USING gin (note gin_trgm_ops);
+CREATE INDEX idx_movie_info ON movie_info USING gin (info gin_trgm_ops);
+CREATE INDEX keyword_idx_1 ON keyword USING gin (keyword gin_trgm_ops);
+CREATE INDEX info_type_idx_1 ON info_type USING gin (info gin_trgm_ops);
+CREATE INDEX company_name_idx_1 ON company_name USING gin (country_code gin_trgm_ops);
