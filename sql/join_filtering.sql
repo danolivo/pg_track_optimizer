fb95ebae8ab96560121d308e21f01bce46f38d42
Andrei V. Lepikhov <lepihov@gmail.com>
2026-01-15 13:53:06 +0100

    Demonstrate that we have a room for improvement in detection inefficient queries.
    Also, invent a filter for portable representation of an explain.

--- sql/join_filtering.sql
+++ sql/join_filtering.sql
@@ -1,3 +1,20 @@
+create or replace function portable_explain_analyze(query text)
+returns table (out_line text) language plpgsql
+as
+$$
+declare
+  line text;
+begin
+  for line in
+    execute 'EXPLAIN (ANALYZE, COSTS OFF, TIMING OFF, SUMMARY OFF, BUFFERS OFF)' || query
+  loop
+    out_line := regexp_replace(line, '\d+kB', 'NNkB', 'g');
+    out_line := regexp_replace(out_line, 'rows=(\d+)\.00', 'rows=\1', 'g');
+    return next;
+  end loop;
+end;
+$$;
+
 CREATE EXTENSION pg_track_optimizer;
 
 -- Cleanup history
@@ -39,10 +56,11 @@ SET enable_material = off;
 -- This query will produce a NestLoop with join filtering
 -- The join condition includes both id match and val match,
 -- where val match acts as a filter
-EXPLAIN (ANALYZE, COSTS OFF, TIMING OFF, SUMMARY OFF, BUFFERS OFF)
+SELECT portable_explain_analyze('
 SELECT * FROM join_outer
 JOIN join_inner ON join_outer.id = join_inner.id
 WHERE join_outer.val + join_inner.val > 5;
+');
 
 -- Check that we tracked the query
 -- The evaluated_nodes should include the join node with filtered tuples
@@ -53,11 +71,50 @@ SELECT
   plan_nodes,
   nexecs
 FROM pg_track_optimizer()
-WHERE query LIKE '%FROM join_outer%';
+WHERE query LIKE '%FROM join_outer%' AND
+  query NOT LIKE '%portable_explain_analyze%' AND
+  query NOT LIKE '%pg_track_optimizer%';
 
 -- Cleanup
 RESET enable_hashjoin;
 RESET enable_mergejoin;
 RESET enable_material;
 
+/*
+ * Just for the demo:
+ * XXX: How to detect such cases: we hash or sort the whole source but use only
+ * minor part of it - no filters, just earlier stop or low usage of a hash table
+ */
+
+-- First pass: no signs of filtered tuples or estimation errors
+SELECT portable_explain_analyze('SELECT * FROM join_inner JOIN join_outer USING (id);');
+
+SELECT
+  ROUND((avg_avg::numeric), 2) AS err,
+  floor(jf_max) AS jf_max, floor(lf_max) AS lf_max,
+  evaluated_nodes en,
+  plan_nodes pn,
+  nexecs nex
+FROM pg_track_optimizer
+WHERE query LIKE '%join_inner JOIN join_outer USING (id)%' AND
+  query NOT LIKE '%portable_explain_analyze%' AND
+  query NOT LIKE '%pg_track_optimizer%';
+
+CREATE INDEX join_outer_id_idx ON join_outer (id);
+-- Second pass: only MergeJoin runtime optimisation detects an estimation error
+SELECT portable_explain_analyze('SELECT * FROM join_inner JOIN join_outer USING (id);');
+
+SELECT
+  ROUND((avg_avg::numeric), 2) AS err,
+  floor(jf_max) AS jf_max, floor(lf_max) AS lf_max,
+  evaluated_nodes en,
+  plan_nodes pn,
+  nexecs nex
+FROM pg_track_optimizer
+WHERE query LIKE '%join_inner JOIN join_outer USING (id)%' AND
+  query NOT LIKE '%portable_explain_analyze%' AND
+  query NOT LIKE '%pg_track_optimizer%';
+
+DROP FUNCTION portable_explain_analyze;
+DROP TABLE join_inner,join_outer;
 DROP EXTENSION pg_track_optimizer;
