4ea3e568c4d259d8b4da4f210299705bbefdd9cd
Andrei V. Lepikhov <lepihov@gmail.com>
2026-02-28 15:12:47 +0100

    Add couple of indexes into the extra_indexes.sql

--- .github/scripts/extra_indexes.sql
+++ .github/scripts/extra_indexes.sql
@@ -7,12 +7,14 @@
 CREATE INDEX aka_name_idx_2 ON aka_name (name);
 CREATE INDEX movie_info_idx_idx_1 ON movie_info_idx (info);
 CREATE INDEX title_idx_1 ON title (production_year);
+CREATE INDEX person_info_note ON person_info (note);
 
 CREATE EXTENSION IF NOT EXISTS pg_trgm;
 
 CREATE INDEX idx_movie_companies ON movie_companies USING gin (note gin_trgm_ops);
 CREATE INDEX cast_info_idx_1 ON cast_info USING gin (note gin_trgm_ops);
 CREATE INDEX idx_movie_info ON movie_info USING gin (info gin_trgm_ops);
+CREATE INDEX movie_info_note ON movie_info USING gin (note gin_trgm_ops);
 CREATE INDEX keyword_idx_1 ON keyword USING gin (keyword gin_trgm_ops);
 CREATE INDEX info_type_idx_1 ON info_type USING gin (info gin_trgm_ops);
 CREATE INDEX company_name_idx_1 ON company_name USING gin (country_code gin_trgm_ops);
